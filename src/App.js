7e9ea8f229130ef27e9f2d6168e0231612b5dc6d
josue Avuh <jkomi2003@gmail.com>
2024-01-29 16:50:03 +0100

    work on Layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React from 'react'
-import {Switch,Route,Link} from 'react-router-dom'
+import './App.css'
+
+import { Routes, Route, Link } from 'react-router-dom';
 import {Layout, Typography, Space} from 'antd'
- 
+import {Homepage,Navbar,Exchanges,Cryptocurrencies,CryptoDetails,News} from './components'
 const App = () => {
   return (
     <div className='app'>
         <div className='navbar'>
-            
+           <Navbar/>
         </div>
+        <div className='main'>
+            <Layout>
+                <div className='routes'>
+                <Routes>
+                    <Route exact path='/' element={<Homepage/>} />
+                    <Route exact path='/exchanges' element={<Exchanges/>} />
+                    <Route exact path='/cryptocurrencies' element={<Cryptocurrencies/>} />
+                    <Route exact path='/crypto/:coinId' element={<CryptoDetails/>} />
+                    <Route exact path='/news' element={<News/>} />
+                </Routes>
+
+                </div>
+            </Layout>
+
+        <div className='footer' level={5}>
+            <Typography.Title level={5} style ={{color:'white', textAlign:'center'}}>
+                Cryptoverse<br/>
+                All rights reserved
+            </Typography.Title>
+            <Space>
+                <Link to='/'>Home</Link>
+                <Link to='/exchanges'>Exchanges</Link>
+                <Link to='/news'>News</Link>
+            </Space>
+            </div>
+        </div>
+
     </div>
   )
 }
