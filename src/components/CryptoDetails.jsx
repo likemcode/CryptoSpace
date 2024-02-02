527ae8de9c32fa455af276249935d9296f20d401
josue Avuh <jkomi2003@gmail.com>
2024-02-02 13:52:25 +0100

    news api connect

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import HTMLReactParser from 'html-react-parser';
+import { useParams } from 'react-router-dom';
 const CryptoDetails = () => {
   return (
     <div>
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
-import { Select, Typography, Row, Col, Avatar, Card } from 'antd'
+import React from 'react';
+import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
+import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
+
+const {Text,Title}= Typography;
 
 const News = () => {
   return (
